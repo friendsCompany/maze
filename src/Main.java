d6bd99df2b98deee4e65910c6c0b6fd6887fd352
kolya <Mkelon83@gmail.com>
2016-06-24 18:00:46 +0300

    thirdCommit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world");
-        System.err.println("error");
-        System.out.println("Bye");
+        int i = 0;
+        while(i < 10) {
+            System.out.println("Hello world");
+            System.err.println("error");
+            System.out.println("Bye");
+            i++;
+        }
     }
+
 }

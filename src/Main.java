18bcf420e941f2b85a1d896fd9fc609216f25d59
kolya <Mkelon83@gmail.com>
2016-06-24 18:06:40 +0300

    fourthCommit

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,9 @@
 
 public class Main {
     public static void main(String[] args){
-        int i = 0;
-        while(i < 10) {
             System.out.println("Hello world");
             System.err.println("error");
             System.out.println("Bye");
-            i++;
-        }
     }
 
 }

f36545151c7bfd0d69185bca2dacf3a72164f107
kolya <Mkelon83@gmail.com>
2016-06-24 21:25:37 +0300

    fifthCommit

---
+++ src/com/friendsCompany/main/Main.java
@@ -0,0 +1,14 @@
+package com.friendsCompany.main;
+
+import com.friendsCompany.view.GameMenu;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        new GameMenu().initMenu();
+    }
+}

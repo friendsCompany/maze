d89a3281a729264f4ab2730db5e48618b2a7f53a
Зеник <omelyan_zenik@mail.ru>
2016-07-15 19:26:14 +0300

    v. 1.0

--- src/com/friendsCompany/main/Main.java
+++ src/com/friendsCompany/main/Main.java
@@ -1,13 +1,30 @@
 package com.friendsCompany.main;
 
 import com.friendsCompany.view.GameMenu;
-
-import javax.swing.*;
-import java.awt.*;
-import java.util.Random;
+import com.sun.j3d.utils.universe.*;
+import com.sun.j3d.utils.geometry.*;
+import javax.media.j3d.*;
+import javax.vecmath.*;
 
 public class Main {
     public static void main(String[] args){
-        new GameMenu().initMenu();
+        SimpleUniverse simpleUniverse = new SimpleUniverse();
+        BranchGroup branchGroup = new BranchGroup();
+        Appearance appearance = new Appearance();
+        appearance.setColoringAttributes(new ColoringAttributes(2f,0f,0f,ColoringAttributes.ALLOW_COLOR_READ));
+        Box box = new Box(100,100,100,appearance);
+        branchGroup.addChild(box);
+
+        Color3f color3f = new Color3f(2,2,0);
+        BoundingBox boundingBox = new BoundingBox(new Point3d(-10,-10,-10),new Point3d(10,10,10));
+        Vector3f vector3f = new Vector3f(5,6,-100);
+        DirectionalLight directionalLight = new DirectionalLight(color3f, vector3f);
+        directionalLight.setInfluencingBounds(boundingBox);
+        branchGroup.addChild(directionalLight);
+
+        simpleUniverse.addBranchGraph(branchGroup);
+        simpleUniverse.getViewingPlatform().setNominalViewingTransform();
+
+        //new GameMenu().initMenu();
     }
 }
